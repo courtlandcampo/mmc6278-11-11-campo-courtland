a76290cb099492a58c32b04fba4cc99990e964e0
courtlandcampo <courtland.campo@ufl.edu>
2022-07-30 19:24:55 -0400

    Established stanza arrays

--- index.js
+++ index.js
@@ -9,11 +9,29 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 const makePoemHTML = ([{title, author, lines}]) => {
-  let titleTag = makeTag("h2")(title) //makes title tag
+  let titleTag = makeTag("h2")(title) //makes title tag + makeTag
 
   let authorNameTag = pipe(makeTag("em"), makeTag("h3"))(`by ${author}`) //make author tag + pipe usage
   
-  return `${titleTag}${authorNameTag}`
+  let linesTag = makeTag("p")(lines)
+
+  let poemArr = [] //will be comprised of multiple stanzas
+  let stanzaArr = [] // will be comprised of lines
+
+  lines.forEach((line,index) => {
+    if(!line){
+      poemArr.push(stanzaArr)
+      stanzaArr = []
+    } else if (index === lines.length - 1) {
+      stanzaArr.push(line)
+      poemArr.push(stanzaArr)
+    } else {
+      stanzaArr.push(line)
+    }
+  })
+
+ 
+  return `${titleTag}${authorNameTag}${poemString}`
 }
 
 // attach a click event to #get-poem
